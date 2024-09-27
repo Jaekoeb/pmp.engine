3a80b6affca12643aad4e4f54f6ab3808cd27fb3
jpertl <pertl.jakob@gmail.com>
2024-09-27 16:15:15 +0200

    Created function that downloads price data for our portfolio.

---
+++ R/download_port_bbg.R
@@ -0,0 +1,76 @@
+#' @title Download Portfolio Data from Bloomberg
+#'
+#' @param id Portfolio ID, as shown in the BBG Terminal under the PRTU Function, default argument is our current portfolio
+#' @param start_date Prices for the portfolio are downloaded starting from this date, default argument is 10 years back
+#'
+#' @return Returns a list with two dataframes. "id" contains fundamental data for each position. "prices" contains historical prices for each position.
+#' @export
+#'
+#' @importFrom dplyr rename full_join left_join
+#' @importFrom tidyr pivot_longer
+#' @importFrom Rblpapi bdh bdp getPortfolio
+#' @importFrom lubridate years
+#'
+download_port_bbg <- function(id = "U31911605-2 Client", start_date = Sys.Date()-years(10)){
+
+  # get the current portfolio
+  current.port <- getPortfolio(id, "Portfolio_Data")
+
+  # get portfolio weights
+  port.weights <- getPortfolio(id, "Portfolio_MWeight")
+  port.weights <- port.weights |> rename("id" = Security)
+
+
+
+  # Price Data --------------------------------------------------------------
+
+  # download price data for all securities in the portfolio
+  df <- bdh(c(port.weights$id, "EURUSD BGN Curncy"), "PX_LAST", start.date = start_date)
+
+
+  # define helper function to rename the columns
+  renamer <- function(df, name){
+    df |>
+      rename(!!name := PX_LAST)
+  }
+
+  # rename "PX_LAST" columns to the element name
+  df <- lapply(names(df), function(x){renamer(df[[x]], x)})
+
+  # merge list into single data frame
+  df <- Reduce(function(x, y){full_join(x, y, by = "date")}, df)
+
+  # remove clutter
+  rm(renamer)
+
+  # pivot df longer
+  df <- df |> pivot_longer(cols = !date, names_to = "id", values_to = "px" )
+
+
+
+  # ID Data -----------------------------------------------------------------
+
+  # download names for all securities
+  df.id <- bdp(c(port.weights$id, "EURUSD BGN Curncy"), c("SECURITY_NAME", "ISSUER", "CRNCY", "MARKET_SECTOR_DES", "LQA_LIQUIDITY_SCORE"))
+
+  df.id$id <- rownames(df.id)
+  rownames(df.id) <- NULL
+
+  df.id <- df.id |> rename(
+    "name" = SECURITY_NAME,
+    "issuer" = ISSUER,
+    "crncy" = CRNCY,
+    "mkt_sector" = MARKET_SECTOR_DES,
+    "lqa_liquidity" = LQA_LIQUIDITY_SCORE
+  )
+
+
+
+  # Join Dataframes ---------------------------------------------------------
+
+  # add the weights to the id dataframe
+  df.id <- left_join(df.id, port.weights, by = "id")
+
+
+  return(list("id" = df.id, "prices" = df))
+}
