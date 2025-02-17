fc128dc47527fda0333393aaecd695e5d2abf587
Luuk Vos <lvos2001@gmail.com>
2025-02-17 10:59:14 +0100

    added function to obtain the HV and IM

---
+++ R/sp500tickers.R
@@ -0,0 +1,14 @@
+#' Data that includes the tickers and the corresponding company names of the SP500.
+#'
+#' @format Data frame with two columns
+#' \describe{
+#' \item{Ticker}{SP500 Company ticker}
+#' \item{Name}{SP500 Company Name}
+#' }
+#'
+#'
+#' @examples
+#' data("sp500tickers")
+#'
+#'
+"sp500tickers"
